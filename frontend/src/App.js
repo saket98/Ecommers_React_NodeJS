4f09855423a75310bd984ff5fa8c648987af8da3
saket98 <saket.pawar98@gmail.com>
2021-03-05 12:18:05 +0530

    Rating and Product Component created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,108 +1,32 @@
-import "./App.css";
-
+import React from 'react';
+import Product from './Components/Product';
+import data from './data';
 function App() {
-	return (
-		<div class="grid-container">
-			<header class="header">
-				<div class="brand">
-					<button onclick="openMenu()">&#9776;</button>
-					<a href="index.html">amazona</a>
-				</div>
-				<div class="header-links">
-					<a href="cart.html">Cart</a>
-					<a href="signin.html">Sign In</a>
-				</div>
-			</header>
-			<aside class="sidebar">
-				<h3>Shopping Categories</h3>
-				<button class="sidebar-close-button" onclick="closeMenu()">
-					x
-				</button>
-				<ul>
-					<li>
-						<a href="index.html">Pants</a>
-					</li>
-
-					<li>
-						<a href="index.html">Shirts</a>
-					</li>
-				</ul>
-			</aside>
-			<main class="main">
-				<div class="content">
-					<ul class="products">
-						<li>
-							<div class="product">
-								<img class="product-image" src="images/d1.jpg" alt="product" />
-								<div class="product-name">
-									<a href="product.html">Slim Shirt</a>
-								</div>
-								<div class="product-brand">Nike</div>
-								<div class="product-price">$60</div>
-								<div class="product-rating">4.5 Stars (10 Reviews)</div>
-							</div>
-						</li>
-						<li>
-							<div class="product">
-								<img class="product-image" src="images/d1.jpg" alt="product" />
-								<div class="product-name">
-									<a href="product.html">Slim Shirt</a>
-								</div>
-								<div class="product-brand">Nike</div>
-								<div class="product-price">$60</div>
-								<div class="product-rating">4.5 Stars (10 Reviews)</div>
-							</div>
-						</li>
-						<li>
-							<div class="product">
-								<img class="product-image" src="images/d1.jpg" alt="product" />
-								<div class="product-name">
-									<a href="product.html">Slim Shirt</a>
-								</div>
-								<div class="product-brand">Nike</div>
-								<div class="product-price">$60</div>
-								<div class="product-rating">4.5 Stars (10 Reviews)</div>
-							</div>
-						</li>
-						<li>
-							<div class="product">
-								<img class="product-image" src="images/d1.jpg" alt="product" />
-								<div class="product-name">
-									<a href="product.html">Slim Shirt</a>
-								</div>
-								<div class="product-brand">Nike</div>
-								<div class="product-price">$60</div>
-								<div class="product-rating">4.5 Stars (10 Reviews)</div>
-							</div>
-						</li>
-						<li>
-							<div class="product">
-								<img class="product-image" src="images/d1.jpg" alt="product" />
-								<div class="product-name">
-									<a href="product.html">Slim Shirt</a>
-								</div>
-								<div class="product-brand">Nike</div>
-								<div class="product-price">$60</div>
-								<div class="product-rating">4.5 Stars (10 Reviews)</div>
-							</div>
-						</li>
-						<li>
-							<div class="product">
-								<img class="product-image" src="images/d1.jpg" alt="product" />
-								<div class="product-name">
-									<a href="product.html">Slim Shirt</a>
-								</div>
-								<div class="product-brand">Nike</div>
-								<div class="product-price">$60</div>
-								<div class="product-rating">4.5 Stars (10 Reviews)</div>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</main>
-			<footer class="footer">All right reserved.</footer>
-		</div>
-	);
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">
+            amazona
+          </a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div>
+          <div className="row center">
+            {data.products.map((product) => (
+              <Product key={product._id} product={product}></Product>
+            ))}
+          </div>
+        </div>
+      </main>
+      <footer className="row center">All right reserved</footer>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/Screens/HomeScreen.js
+++
@@ -1,7 +0,0 @@
-import react from "react";
-import { link } from "react-router-dom";
-import data from "../data"
-
-function Homescreen(props) {
-    
-}
