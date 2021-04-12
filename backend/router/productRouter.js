a436dcb6ac7e1e28cdf1bc0ca905eca89500f202
saket98 <saket.pawar98@gmail.com>
2021-04-12 23:33:14 +0530

    product added to the MongoDB

--- backend/router/productRouter.js
+++ backend/router/productRouter.js
@@ -1,16 +1,33 @@
 const express = require("express");
 const Product = require("../models/productModel");
-const asyncHandler = require('express-async-handler');
+const asyncHandler = require("express-async-handler");
 const data = require("../data");
 
 const productRouter = express.Router();
 
 productRouter.get(
-	"/:id",
+	"/",
 	asyncHandler(async (req, res) => {
+		const products = await Product.find({});
+		res.send(products);
+	})
+);
+
+productRouter.get(
+	"/seed",
+	asyncHandler(async (req, res) => {
+        await Product.remove({})
 		const createdProducts = await Product.insertMany(data.products);
 		res.send({ createdProducts });
 	})
 );
 
+productRouter.get(
+	"/:id",
+	asyncHandler(async (req, res) => {
+		const product = await Product.findById(req.params.id);
+		if (product) res.send({ product });
+		else res.status(404).send({ message: "Product not found" });
+	})
+);
 module.exports = productRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const { products } = require("./data");
-const data = require("./data");
 const productRouter = require("./router/productRouter");
 const userRouter = require("./router/userRouter");
 
@@ -20,18 +19,7 @@ mongoose
 	})
 	.catch((err) => console.log(err.message));
 
-app.get("/api/products/:id", (req, res) => {
-	const product = products.find((x) => x._id === req.params.id);
-	if (product) {
-		res.send(product);
-	} else {
-		res.status(404).send({ message: "Product not found" });
-	}
-});
 
-app.get("/api/products", (req, res) => {
-	res.send(data.products);
-});
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
