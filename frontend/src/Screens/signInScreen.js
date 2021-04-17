fac7bed2d7430829737bba7e4686ca1c3b3dd61f
saket98 <saket.pawar98@gmail.com>
2021-04-17 18:11:53 +0530

    Error removed

--- frontend/src/Screens/signInScreen.js
+++ frontend/src/Screens/signInScreen.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { signin } from "../Action/userAction";
-import LoadingBox from '../Components/LoadingBox';
-import MessageBox from '../Components/MessageBox';
+import LoadingBox from "../Components/LoadingBox";
+import MessageBox from "../Components/MessageBox";
 
 function SigninScreen(props) {
 	const [email, setEmail] = useState("");
