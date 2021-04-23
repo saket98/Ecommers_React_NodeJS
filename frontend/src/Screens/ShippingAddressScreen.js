802ae3f413cc811d99eb17ae023be4565c1896c5
saket98 <saket.pawar98@gmail.com>
2021-04-23 22:57:32 +0530

    ShippingScreen added

---
+++ frontend/src/Screens/ShippingAddressScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CheckoutSteps from '../Components/CheckoutSteps'
+
+export default function ShippingAddressScreen() {
+    return (
+        <div>
+            <CheckoutSteps step1 step2></CheckoutSteps>
+        </div>
+    )
+}
